b5e59d79737e910f4e22156defae2178693596c7
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-19 15:56:33 +0300

    feat: configured eslint, prettier and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/no-anonymous-default-export': [
+        'error',
+        {
+          allowArray: true,
+          allowArrowFunction: true,
+          allowAnonymousClass: true,
+          allowAnonymousFunction: true,
+          allowCallExpression: true,
+          allowLiteral: true,
+          allowObject: true,
+        },
+      ],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
