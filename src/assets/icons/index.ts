2ec1868a447ac9892ad5368e1f8c373141491520
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-20 18:08:06 +0300

    feat: add icons and implement icon component

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+import { Loader2Icon as Loader } from 'lucide-react';
+
+export { default as Logo } from './logo.svg';
+export { default as Search } from './search.svg';
+export { default as User } from './user.svg';
+export { default as Cart } from './cart.svg';
+export { default as Favorites } from './favorites.svg';
+
+export { Loader };
