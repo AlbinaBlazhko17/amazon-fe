bd8957d44642739b72197df9b8b4193e4e77ed96
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-20 18:16:46 +0300

    feat: add public api to components folder

---
+++ src/components/common/index.ts
@@ -0,0 +1,4 @@
+export { Icon, type TIcons } from './Icon';
+export { SearchInput } from './Inputs';
+export { Text } from './Text';
+export { ExternalLink, NavLink } from './NavLink';
---
+++ src/components/ui/index.ts
@@ -0,0 +1,2 @@
+export { Input, type InputProps } from './input';
+export { Button, type ButtonProps, buttonVariants } from './button';
