6ba1ef4ec361dde7b61a9a5ec48f9c52ab0ea990
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-20 18:11:33 +0300

    feat: setup svgr

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,38 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find(
+			(rule: { test?: { test?: (fileName: string) => boolean } }) => rule.test?.test?.('.svg')
+		);
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/ // *.svg?url
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+				use: ['@svgr/webpack']
+			}
+		);
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
+	experimental: {
+		turbo: {
+			rules: {
+				'*.svg': {
+					loaders: ['@svgr/webpack'],
+					as: '*.js'
+				}
+			}
+		}
+	},
 	eslint: {
 		ignoreDuringBuilds: true
 	}
