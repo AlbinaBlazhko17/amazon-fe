d6cd56d56164d9bacd3210ba25045b56baf23708
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-23 10:00:10 +0300

    feat: add animations to submenu with categories

--- src/features/subnav-categories/SubnavCategories.tsx
+++ src/features/subnav-categories/SubnavCategories.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 import { Icon, Text } from '@/components/common';
@@ -10,32 +11,48 @@ export const SubnavCategories = () => {
 	const { categories } = useSubnavCategories();
 
 	return (
-		<div
+		<motion.div
+			initial={'closed'}
+			animate={categories.length > 0 ? 'open' : 'closed'}
+			variants={{
+				open: { opacity: 1, height: 'auto' },
+				closed: { opacity: 0, height: 0 }
+			}}
+			transition={{ duration: 0.3 }}
 			className={
 				'hidden w-full items-center justify-center gap-4 bg-neutral-600 py-2 lg:flex lg:px-10 xl:px-20 2xl:px-40'
 			}
 		>
 			<ul className={'first: flex items-center text-neutral-300 lg:gap-3 xl:gap-5 2xl:gap-12'}>
-				{categories.length > 0 &&
-					categories.map(({ id, name, icon, slug }, i) => (
-						<div key={id} className={'flex items-center lg:gap-3 xl:gap-5 2xl:gap-12'}>
-							<li>
-								<Link
-									href={`/categories/${slug}`}
-									className={'flex items-center gap-1 hover:text-neutral-200'}
-								>
-									<Icon name={icon} className={'size-6 text-neutral-200'} />
-									<Text as={'span'} className={'ml-2 text-sm'}>
-										{name}
-									</Text>
-								</Link>
-							</li>
-							<div
-								className={`${i === categories.length - 1 ? 'hidden' : 'block'} h-5 w-[2px] rounded-full bg-neutral-400`}
-							></div>
-						</div>
-					))}
+				{categories.map(({ id, name, icon, slug }, i) => (
+					<motion.div
+						key={id}
+						initial={'closed'}
+						animate={categories.length > 0 ? 'open' : 'closed'}
+						variants={{
+							open: { opacity: 1, y: 0 },
+							closed: { opacity: 0, y: -10 }
+						}}
+						transition={{ duration: 0.3, delay: (i + 1) * 0.1 }}
+						className={'flex items-center lg:gap-3 xl:gap-5 2xl:gap-12'}
+					>
+						<li>
+							<Link
+								href={`/categories/${slug}`}
+								className={'flex items-center gap-1 hover:text-neutral-200'}
+							>
+								<Icon name={icon} className={'size-6 text-neutral-200'} />
+								<Text as={'span'} className={'ml-2 text-sm'}>
+									{name}
+								</Text>
+							</Link>
+						</li>
+						<div
+							className={`${i === categories.length - 1 ? 'hidden' : 'block'} h-5 w-[2px] rounded-full bg-neutral-400`}
+						></div>
+					</motion.div>
+				))}
 			</ul>
-		</div>
+		</motion.div>
 	);
 };
