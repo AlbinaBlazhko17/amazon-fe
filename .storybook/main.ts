0ab96cf6bcd68003932938e89686f44774d21be9
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-28 11:54:48 +0300

    feat: setup svgr in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,35 @@ const config: StorybookConfig = {
 		name: '@storybook/nextjs',
 		options: {}
 	},
-	staticDirs: ['../public']
+	staticDirs: ['../public'],
+	webpackFinal: async config => {
+		const fileLoaderRule = config.module?.rules?.find(rule =>
+			(rule as { test?: RegExp })?.test?.test('.svg')
+		) as {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			[key: string]: any;
+		};
+
+		config.module?.rules?.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/
+			},
+
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: {
+					not: [...(fileLoaderRule?.resourceQuery?.not || []), /url/]
+				},
+				use: ['@svgr/webpack']
+			}
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	}
 };
 export default config;
