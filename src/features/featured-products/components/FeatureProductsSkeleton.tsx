23fac83d7b6d6d4dc02e63ac6eff8c3262b1faed
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-06-12 13:19:48 +0300

    feat: add skeleton on loading

---
+++ src/features/featured-products/components/FeatureProductsSkeleton.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from '@/components/ui';
+
+export const FeatureProductsSkeleton = () => (
+	<>
+		<Skeleton className={'delay-75 lg:col-span-4'} />
+		<Skeleton className={'row-start-4 delay-100 lg:col-span-2 lg:row-start-2'} />
+		<Skeleton
+			className={'row-start-5 delay-150 lg:col-span-2 lg:col-start-3 lg:row-span-2 lg:row-start-2'}
+		/>
+		<Skeleton className={'delay-200'} />
+		<Skeleton className={'delay-300'} />
+	</>
+);
--- src/features/featured-products/components/index.ts
+++ src/features/featured-products/components/index.ts
@@ -1,3 +1,4 @@
 export { BigBlock } from './BigBlock';
 export { Hero } from './Hero';
 export { SmallBlock } from './SmallBlock';
+export { FeatureProductsSkeleton } from './FeatureProductsSkeleton';
