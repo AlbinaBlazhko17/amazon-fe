e007ee1e30c1b848ffb719ed1b6ec1a0f6dbfe48
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-06-12 13:18:40 +0300

    feat: add skeleton component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { Input, type InputProps } from './input';
 export { Button, type ButtonProps, buttonVariants } from './button';
 export { Toaster } from './sonner';
+export { Skeleton } from './skeleton';
